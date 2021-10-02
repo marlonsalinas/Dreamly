cee8af18710cf2c673eb74657ce6c0c62e51cc43
marlonsalinas <msal_33@yahoo.com>
2021-10-02 13:11:24 -0500

    basic app

---
+++ server.js
@@ -0,0 +1,32 @@
+// Dependencies
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const PORT = 5000
+
+// Mongoose Database Connection //
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+//Database connection error/success, defined callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+//Routes
+app.get('/dreamly', (req, res) => {
+    res.render('home.ejs');
+});
+
+// Index
+app.get('/dreamly/logs0')
+
+
+// Listen
+app.listen(PORT, () => {
+    console.log("I'm listening to what Josie said on port:", PORT);
+})

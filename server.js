0e824445436058728aa7c8c847502c05513612c2
marlonsalinas <msal_33@yahoo.com>
2021-10-02 17:55:44 -0500

    ignored env

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
 //Database connection error/success, defined callback functions for various events
 const db = mongoose.connection
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
